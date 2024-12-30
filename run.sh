536917ecec4325ab85cdca32b68739d45cbb84c3
OlgaOvcharenko <44943723+OlgaOvcharenko@users.noreply.github.com>
2024-12-30 15:03:51 +0100

    Update run.sh

--- run.sh
+++ run.sh
@@ -30,14 +30,14 @@
 #                     --visualize_ckpts 100
 
 # # pancreas
-python main_optim.py --dname 'Pancreas_our' --n_repeat 1 --moco_k 2048 --moco_m 0.999 --moco_t 0.07 \
-                     --block_level 1 --lat_dim 128 --symmetric True \
-                     --select_hvg 4000 \
-                     --knn 10 --alpha 0.5 --augment_set 'int' \
-                     --lr 1e-4 --optim Adam --weight_decay 1e-5 --epochs 100 --batch_size 256\
-                     --anchor_schedule 2 --fltr 'gmm' --yita 0.5 \
-                     --save_freq 10 --start_epoch 0 --workers 6 --init 'uniform' \
-                     --visualize_ckpts 99   
+# python main_optim.py --dname 'Pancreas_our' --n_repeat 1 --moco_k 2048 --moco_m 0.999 --moco_t 0.07 \
+#                      --block_level 1 --lat_dim 128 --symmetric True \
+#                      --select_hvg 4000 \
+#                      --knn 10 --alpha 0.5 --augment_set 'int' \
+#                      --lr 1e-4 --optim Adam --weight_decay 1e-5 --epochs 100 --batch_size 256\
+#                      --anchor_schedule 2 --fltr 'gmm' --yita 0.5 \
+#                      --save_freq 10 --start_epoch 0 --workers 6 --init 'uniform' \
+#                      --visualize_ckpts 99   
 
 # # immune
 # python main.py --dname 'ImmHuman_our' --n_repeat 2 --moco_k 2048 --moco_m 0.999 --moco_t 0.07 \
@@ -59,12 +59,12 @@ python main_optim.py --dname 'Pancreas_our' --n_repeat 1 --moco_k 2048 --moco_m 
 #                     --save_freq 10 --start_epoch 0 --workers 6 --init 'uniform' \
 #                     --visualize_ckpts 100
 
-# # immune atlas
-# python main.py --dname 'ImmuneAtlas_our' --n_repeat 2 --moco_k 2048 --moco_m 0.999 --moco_t 0.07 \
-#                     --block_level 1 --lat_dim 128 --symmetric True \
-#                     --select_hvg 4000 \
-#                     --knn 10 --alpha 0.5 --augment_set 'int' \
-#                     --lr 1e-4 --optim Adam --weight_decay 1e-5 --epochs 100 --batch_size 256\
-#                     --anchor_schedule 5 --fltr 'gmm' --yita 0.5 \
-#                     --save_freq 10 --start_epoch 0 --workers 6 --init 'uniform' \
-#                     --visualize_ckpts 100
+# immune atlas
+python main.py --dname 'ImmuneAtlas_our' --n_repeat 2 --moco_k 2048 --moco_m 0.999 --moco_t 0.07 \
+                    --block_level 1 --lat_dim 128 --symmetric True \
+                    --select_hvg 4000 \
+                    --knn 10 --alpha 0.5 --augment_set 'int' \
+                    --lr 1e-4 --optim Adam --weight_decay 1e-5 --epochs 100 --batch_size 256\
+                    --anchor_schedule 5 --fltr 'gmm' --yita 0.5 \
+                    --save_freq 10 --start_epoch 0 --workers 6 --init 'uniform' \
+                    --visualize_ckpts 100
