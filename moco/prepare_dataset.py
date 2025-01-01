5ae7f88dc5048730e06c5e49acb4906f6547524f
OlgaOvcharenko <ovcharenko.folga@gmail.com>
2025-01-02 00:57:57 +0100

    Fix batch name

--- moco/prepare_dataset.py
+++ moco/prepare_dataset.py
@@ -186,7 +186,7 @@ def prepare_Pancreas_our(data_root):
     return X, gene_name, cell_name, df_meta
 
 def prepare_ImmuneAtlas_our(data_root):
-    batch_key = 'batch'
+    batch_key = 'batchlb'
     label_key = 'cell_type'
 
     # ensure row is gene
